4c44fcf72a8e584caf44a76695680e037208d287
CReis93 <100361637+CReis93@users.noreply.github.com>
2022-05-20 07:31:01 +0200

    Add files via upload

--- ML_project_code.py
+++ ML_project_code.py
@@ -12,21 +12,25 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn import preprocessing
 label_encoder = preprocessing.LabelEncoder()
 import matplotlib.pyplot as plt
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.datasets import make_classification
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
 
 #%%--------------------------------loading data--------------------------------
 
 # import data as dataframe
+
 df = pd.read_csv('Master_day6.csv',low_memory=False)
 
 #%%--------------------------------data processing-----------------------------
 
-# drop the raw with concentration value of DMSO
+# drop the raw with concentration value of DMSO (solute test)
 df = df[df["Nom_Conc"].str.contains("DMSO") == False]
 
 # change type for conc_name
@@ -35,8 +39,13 @@ df['Nom_Conc'] = pd.to_numeric(df['Nom_Conc'])
 df.info() # checking the columns
 
 # keeping only the needed columns
-data = df[['X','Y','Area','Mean','Min','Max','Perimeter','Major','Minor','Angle','step_length','step_speed','abs_angle','rel_angle','jell','name','Nom_Conc']].copy()
+data = df[['Area','Perimeter','Major','Minor','step_length','step_speed','abs_angle','rel_angle','Nom_Conc']].copy()
 data.info()
+
+#%%---take 5 random line for the report & export it (no use for the code)------
+
+#sampleDF = data.sample(n = 5)
+#sampleDF.to_csv('sampleDF.csv')
 
 #%%-----------------------delet infinite and nan values------------------------
 
@@ -50,56 +59,148 @@ data_noNAN = data.dropna(axis=0)
 # check again that no raw has NaN values
 data_noNAN.isna().values.any()
 
-#%%---------------------------------classification-----------------------------
 
-# data split
+#%%-------------------------------represent data-------------------------------
+
+X1 = data_noNAN[['step_speed']].copy()
+X2 = data_noNAN[['step_length']].copy()
+c = data_noNAN[['Nom_Conc']].copy()
+c = label_encoder.fit_transform(c) 
+
+fig = plt.figure()
+
+plt.scatter(X1, X2, c=c)
+
+# Set figure title and axis labels
+plt.title('step_speed vs abs_angle for each measurement point')
+plt.xlabel("step_speed [pixel/sec]")
+plt.ylabel("step_length [pixel]")
+
+#%%------------------------------split the data--------------------------------
+
+# select the variables
+X = data_noNAN[['step_speed','Major']].copy()
 Y = data_noNAN[['Nom_Conc']].copy()
-X = data_noNAN[['step_speed']].copy()
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=7000, train_size=20000)
-X_test, X_val, y_test, y_val = train_test_split(X_train, y_train, test_size=7000, train_size=7000)
-
-print(len(X_train), len(X_test), len(X_val))
+# split the dataset for train, validation and test
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, train_size=0.7)
+X_test, X_val, y_test, y_val = train_test_split(X, Y, test_size=0.15, train_size=0.15)
 
 # label encoding in order to normalise the target variable
 label_encoder = preprocessing.LabelEncoder()
 y_train = label_encoder.fit_transform(y_train)
 y_test = label_encoder.fit_transform(y_test)
-y_val = label_encoder.fit_transform(y_val)
 
-# RandomForestClassifier
+print(len(X_train), len(X_test), len(X_val))
+
+#%%--------------------------- train and test the model------------------------
+#with RandomForestClassifier and no hyperparam 
+
 rfc = RandomForestClassifier()
+
+# fit the data (training)
 rfc.fit(X_train, y_train)
 
-RFC_val = rfc.predict(X_val)
+# predict after training on test set
+RFC_test = rfc.predict(X_test)
 
-print('\nMatrix confusion for RandomForestClassifier :')
-print(confusion_matrix(y_val, RFC_val))
+# print the matrix and the accuracy
+print('\nMatrix confusion for RandomForestClassifier (without HP) :')
+print(confusion_matrix(y_test, RFC_test))
 
-acc_RFC = accuracy_score(y_val, RFC_val)
-print(f'\nThe accuracy of the model RandomForestClassifier is {acc_RFC:.1%}')
+acc_RFC = accuracy_score(y_test, RFC_test)
+print(f'\nThe accuracy of the model RandomForestClassifier is (without HP) {acc_RFC:.1%}')
 
-print(RFC_val)
+#%%--------------------------------- search HP---------------------------------
 
-#%%
+# initiate the gridsearch (inspired from https://www.kaggle.com/code/sociopath00/random-forest-using-gridsearchcv/notebook)
+params = { 
+    'n_estimators': [150],
+    'max_features': ['auto','sqrt'],
+    'max_depth' : [4,8],
+    'criterion' :['gini', 'entropy']
+}
+gsc = GridSearchCV(rfc, params, cv=5)
 
+gsc.fit(X_train, y_train)
 
+#Results key
+print('\nResults keys :')
+sorted(gsc.cv_results_.keys())
 
-#%%--------------------------------data visualization--------------------------
+# print the best hyperparameters
+print('\nBest params :')
+print(gsc.best_params_)
 
-#x = data.iloc[0:1552]
+# and score
+print('\nBest score :')
+print(gsc.best_score_)
 
-#fig, plt = plt.subplots()
-#plt.scatter(x['X'],x['Y'])
+#Best estimator
+print('\nBest estimator :')
+gsc.best_estimator_
 
+#%%--------------------------- train and test the model------------------------
+#with DecisionTreesClassifier and no hyperparam 
 
-# create a DF for each 
-#j = 0 #initiate new idex position
+dtc = DecisionTreeClassifier(min_impurity_decrease=0,criterion='gini', splitter='best', min_samples_split=2, min_samples_leaf = 1,min_weight_fraction_leaf=0) 
 
-#for i in range (1,len(data)):
-    #if data(data[i]['Nom_Conc']) == 0:
-        #Conc_0[j] = data[i]
+# fit the data (training)
+dtc.fit(X_train, y_train)
 
-        
+# predict after training on test set
+dtc_test = dtc.predict(X_test)
 
+# print the matrix and the accuracy
+print('\nMatrix confusion for RandomForestClassifier (without HP) :')
+print(confusion_matrix(y_test, dtc_test))
+
+acc_RFC = accuracy_score(y_test, dtc_test)
+print(f'\nThe accuracy of the model RandomForestClassifier is (without HP) {acc_RFC:.1%}')
+
+#%%--------------------------------- search HP---------------------------------
+
+parameters = {'max_features': ['auto', 'sqrt', 'log2'],
+              'ccp_alpha': [0.1, .01, .001],
+              'max_depth' : [5, 6, 7, 8, 9]
+             }
+
+gsc1 = GridSearchCV(dtc, parameters,cv=5)
+
+# fit the data (training)
+gsc1.fit(X_train,y_train)
+
+#Results key
+print('\nResults keys :')
+sorted(gsc1.cv_results_.keys())
+
+# print the best hyperparameters
+print('\nBest params :')
+print(gsc1.best_params_)
+
+# and score
+print('\nBest score :')
+print(gsc1.best_score_)
+
+#Best estimator
+print('\nBest estimator :')
+gsc1.best_estimator_
+
+#%%--------------------------- train and test the model------------------------
+#with DecisionTreesClassifier and hyperparam 
+
+dtc = DecisionTreeClassifier(min_impurity_decrease=0,criterion='gini', splitter='best', min_samples_split=2, min_samples_leaf = 1,min_weight_fraction_leaf=0,ccp_alpha = 0.001, max_depth= 9,max_features ='sqrt') 
+
+# fit the data (training)
+dtc.fit(X_train, y_train)
+
+# predict after training on test set
+dtc_test = dtc.predict(X_test)
+
+# print the matrix and the accuracy
+print('\nMatrix confusion for RandomForestClassifier (without HP) :')
+print(confusion_matrix(y_test, dtc_test))
+
+acc_RFC = accuracy_score(y_test, dtc_test)
+print(f'\nThe accuracy of the model RandomForestClassifier is (without HP) {acc_RFC:.1%}')
 
